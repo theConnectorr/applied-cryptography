5c60b510d9c0ab7f2731241bd0a6a037a2891d73
theworstkhoi <kylekhoitv@gmail.com>
2025-04-09 20:02:00 +0700

    feat: init react folder structure, prettier and eslint config

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center w-screen h-screen text-9xl">
+      <div>Hello mother</div>
+    </div>
+  )
+}
+
+export default App
